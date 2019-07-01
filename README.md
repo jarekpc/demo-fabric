b5c50fd69feb68f60607bee8f1b23118c3280f7f
jarek <jaroslaw.zyzak@ailleron.com>
2019-07-01 15:40:51 +0200

    added a all project

---
+++ README.md
@@ -0,0 +1,7 @@
+mvn io.fabric8:fabric8-maven-plugin:3.5.19:setup
+
+mvn io.fabric8:fabric8-maven-plugin:3.5.42:setup
+
+mvn io.fabric8:fabric8-maven-plugin:4.1.0:setup
+
+
---
+++ src/main/java/com/example/demofabric/DemoFabricApplication.java
@@ -0,0 +1,13 @@
+package com.example.demofabric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFabricApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoFabricApplication.class, args);
+    }
+
+}
